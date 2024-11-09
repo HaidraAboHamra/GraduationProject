b0c7f46f4d8ba831062ce5c4045ec5cb6eeb76b3
Haidra <haidraabohamra722@gmail.com>
2024-11-09 20:43:57 +0300

    Add College and lecture entites

---
+++ GraduationProject/Entities/College.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GraduationProject.Entities
+{
+    public class College
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AcademicYear { get; set; }
+       
+        
+        
+        //Navigation
+        public List<Lecture> Lectures { get; set; }
+    }
+}
---
+++ GraduationProject/Entities/Lecture.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GraduationProject.Entities
+{
+    public class Lecture
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public int CollegeId { get; set; }
+        [ForeignKey("CollegeId")]
+        public College College { get; set; }
+    }
+}
