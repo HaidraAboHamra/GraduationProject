8d83b7633753c7c9ffd7e2d458c4c335e2305dce
Haidra <haidraabohamra722@gmail.com>
2024-11-11 00:08:50 +0300

    Add MAUI for Android And Desktop App

---
+++ Presention/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Presention
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Presention/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Presention
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
